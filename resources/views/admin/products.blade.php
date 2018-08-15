d1660fe8baf7f3d7ec8d4e2c6f825c08521c3e08
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2018-08-15 06:43:19 -0700

    Add product table

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-this is admin home page
+<br>
+<div class="content">
+	<h3>Product Lists</h3>
+
+	<table>
+		<tr>
+			<th>Product ID</th>
+			<th>Product Name</th>
+			<th>Price</th>
+			<th>Last Updated</th>
+		</tr>
+	</table>
+</div>
+
 
 @endsection
